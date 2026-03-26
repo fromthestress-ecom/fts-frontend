d617f8e5989c35a25f921076b3e99e0b575570ca
nghiamh123 <73655534+nghiamh123@users.noreply.github.com>
2026-03-26 16:01:08 +0700

    feat: Implement root layout with global contexts, internationalization, analytics, and core UI components.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -16,6 +16,7 @@ import { NavigationOverlay } from "@/components/NavigationOverlay";
 import { Oswald, DM_Sans } from "next/font/google";
 
 const GA_ID = "G-HWRRD05NNB";
+const FB_PIXEL_ID = "1164538798780806";
 import "../globals.css";
 
 const oswald = Oswald({
@@ -106,8 +107,30 @@ export default async function RootLayout({
         <Script id="gtag-init" strategy="afterInteractive">
           {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${GA_ID}');`}
         </Script>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
         <NextIntlClientProvider messages={messages} locale={locale}>
           <ThemeProvider>
             <AuthProvider>
